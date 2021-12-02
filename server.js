9a97025058fe0075c280383fda8b6b7c02abfb9c
Mark Bozsoki <mark.bozsoki@stobag.com>
2021-12-02 17:11:52 +0100

    Force rebuild

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 8080;
 
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.listen(port, () => console.log(`Server started at port: ${port}`));
