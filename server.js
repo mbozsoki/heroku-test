2a2fe352593e8cccd1ec415a9938e77ff5943bd8
Mark Bozsoki <mark.bozsoki@stobag.com>
2021-12-02 17:15:33 +0100

    Force rebuild

--- server.js
+++ server.js
@@ -7,4 +7,12 @@ const port = process.env.PORT || 8080;
 //When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
+app.get("/api", (req, res) => {
+    res.json({ message: '👋 from Express!' });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(port, () => console.log(`Server started at port: ${port}`));
